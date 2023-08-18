9016ebd75f8f509ace50bca78b43ba700403ef50
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-08-18 08:33:34 -0700

    adds lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name="lib/logfile", file_content="log 1: 5 bananas added"):
+    with open(f"{file_name}.txt", mode="w", encoding="utf-8") as logfile:
+        logfile.write(f"{file_content}")
 
-def append_file(file_name, append_content):
-    pass
 
-def read_file(file_name):
-    pass
+def append_file(file_name="lib/logfile", append_content="log 2: 3 bananas subtracted"):
+    with open(f"{file_name}.txt", mode="a", encoding="utf-8") as logfile:
+        logfile.write(f"{append_content}")
+
+
+def read_file(file_name="lib/logfile"):
+    with open(f"{file_name}.txt", encoding="utf-8") as logfile:
+        return logfile.read()
+
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added")
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+read_file(file_name="logfile")
